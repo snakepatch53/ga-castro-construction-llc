3b8a0843240c940263f5d152d75cc4e9d5dba9ce
snakepatch53 <snakepatch53@gmail.com>
2024-01-19 17:16:16 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	renamed:    dist/assets/index-AKsIzQFG.js -> dist/assets/index-IpG5L9QV.js
    	renamed:    dist/assets/index-1ZV79Kdr.css -> dist/assets/index-L0eD3DVz.css
    	modified:   dist/index.html
    	modified:   src/landing.components/SectionContact.jsx
    	modified:   src/landing.views/ServicePaint.jsx
    
    Co-authored-by: josearizaga009 <josearizaga009@users.noreply.github.com>

--- src/landing.components/SectionContact.jsx
+++ src/landing.components/SectionContact.jsx
@@ -6,7 +6,7 @@ import socials from "./../mooks/social.json";
 
 export default function SectionContact() {
     return (
-        <div className="flex flex-col items-center w-full gap-5 lg:flex-row">
+        <div className="flex flex-col items-center w-full gap-5 lg:flex-row font-title2">
             <div className="flex-1 flex flex-col gap-10 items-center justify-center bg">
                 <h3 className="text-3xl ">Contact Us</h3>
                 <div className="px-5 sm:px-10">
@@ -24,7 +24,9 @@ export default function SectionContact() {
             </div>
             <div className="flex-1 flex w-full justify-center">
                 <div className="flex flex-col w-[600px] lg:w-full px-8 py-5 gap-3 rounded-lg bg-[#171640]">
-                    <h3 className=" text-xl pb-3 text-[var(--color1-txt1)]">Send us a message</h3>
+                    <h3 className=" text-xl pb-1 pl-3 text-[var(--color1-txt1)]">
+                        Send us a message
+                    </h3>
                     <Input placeholder="Your name" />
                     <Input placeholder="Your e-mail" />
                     <Input placeholder="Address home" />
--- src/landing.views/ServicePaint.jsx
+++ src/landing.views/ServicePaint.jsx
@@ -1,6 +1,8 @@
 import AnimatedElement from "../components/AnimatedElement";
 import Button from "../landing.components/Button";
 import SectionContact from "../landing.components/SectionContact";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightLong } from "@fortawesome/free-solid-svg-icons";
 
 export default function ServicePaint() {
     return (
@@ -9,16 +11,24 @@ export default function ServicePaint() {
                 <div className="container">
                     <div className="flex flex-col px-20 pt-20">
                         <div className="flex flex-col gap-5 w-full lg:flex-row">
-                            <div className="flex-1 flex flex-col gap-5 justify-center">
-                                <h3 className="text-4xl text-balance">
-                                    expert painting{" "}
-                                    <span className="text-[var(--color3-txt1)]">services</span>
-                                </h3>
-                                <span className="text-3xl">
-                                    Flawless finishes and transformative results.
-                                </span>
-                                <Button to="/about-us" style="3" type="2" className="mt-2 mr-auto">
-                                    <span>Contact us now</span> <span>&rarr;</span>
+                            <div className="flex-1 flex flex-col gap-5 justify-center font-title">
+                                <div className="flex flex-col gap-5 pl-3">
+                                    <h3 className="text-6xl text-balance ">
+                                        expert painting{" "}
+                                        <span className="text-[var(--color3-txt1)] ">services</span>
+                                    </h3>
+                                    <span className="text-4xl ">
+                                        Flawless finishes and transformative results.
+                                    </span>
+                                </div>
+                                <Button
+                                    to="/about-us"
+                                    style="3"
+                                    type="2"
+                                    className="mt-2 mr-auto gap-3 px-5 items-center"
+                                >
+                                    <span className="text-xl font-title">Contact us now</span>{" "}
+                                    <FontAwesomeIcon className="text-xl " icon={faRightLong} />
                                 </Button>
                             </div>
 
@@ -28,31 +38,30 @@ export default function ServicePaint() {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex pb-5 pt-2 justify-end font-content font-bold text-x">
+                        <div className="flex pb-5 pt-2 justify-end  font-title2 text-xl">
                             <span>Selection of paint to suit the client.</span>
                         </div>
                     </div>
                 </div>
             </section>
-            <section>
-                <div className=" container ">
-                    <div className="relative flex  w-full  h-[500px] items-center justify-center overflow-hidden rounded-md">
-                        <img className="w-full min-h-96" src="/img/services.jpg" />
-                        <div className="absolute inset-0 backdrop-blur-sm" />
-                        <div className="absolute flex items-center  px-5 py-10 gap-2 mx-10 sm:mx-20 md:mx-40 rounded-2xl bg-[#000000]/60 text-[var(--color1-txt1)] shadow-xl ">
-                            <div className="flex flex-col max-w-[420px] gap-5 items-center">
-                                <h3 className="text-2xl">Transform your interiors</h3>
-                                <p className="text-xl text-center">
-                                    Each space in your home Will come to life when you choose its
-                                    colors correctly.
-                                </p>
-                            </div>
-                        </div>
+            <section className="relative">
+                <img
+                    className="absolute inset-0 w-full h-full object-cover"
+                    src="/img/services.jpg"
+                />
+                <div className="absolute inset-0 backdrop-blur-sm" />
+                <div className=" container flex  w-full  h-[500px] items-center justify-center font-title2">
+                    <div className="relative z-10 flex items-center  px-2 py-10 gap-5 mx-10 sm:mx-20 md:mx-40 rounded-2xl bg-[#000000]/60 text-[var(--color1-txt1)] shadow-xl  flex-col max-w-[550px]">
+                        <h3 className="text-4xl">Transform your interiors</h3>
+                        <p className="text-2xl text-center">
+                            Each space in your home Will come to life when you choose its colors
+                            correctly.
+                        </p>
                     </div>
                 </div>
             </section>
             <section className="p-[var(--padding)]">
-                <div className="container w-full">
+                <div className="container w-full mt-10  lg:mt-32">
                     <SectionContact />
                 </div>
             </section>
